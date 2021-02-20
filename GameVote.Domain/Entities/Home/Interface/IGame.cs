c2ba66c045d577802db52296401592ed1df5968c
Олег <oleg_vampiriys@mail.ru>
2021-02-20 15:56:14 +0300

    Главная страница отображает информацию об играх, но без картинок

--- GameVote.Domain/Entities/Home/Interface/IGame.cs
+++ GameVote.Domain/Entities/Home/Interface/IGame.cs
@@ -12,6 +12,7 @@ namespace GameVote.Domain.Entities.Home.Interface
         float PriceRelise { get; set; }
         float PriceNow { get; set; }
         string UrlGame { get; set; }
-        string[] ImgGame { get; set; }
+        string ImgGame { get; set; }
+        string[]? ScreenGame { get; set; }
     }
 }
