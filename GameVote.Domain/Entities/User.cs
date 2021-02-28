81f464263acd4e9c1bdd2c506d02b8d9a3f0bb00
ilya <illf.yar@googlemail.com>
2021-02-28 17:51:03 +0300

    Добавил в сервис БД метод получения модели игр для титульной страницы

--- GameVote.Domain/Entities/User.cs
+++ GameVote.Domain/Entities/User.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
-using GameVote.Domain.Entities.Home.Interface;
 
 namespace GameVote.Domain.Entities
 {
-    public class User : IdentityUser
+    public class User
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
     }
 }
