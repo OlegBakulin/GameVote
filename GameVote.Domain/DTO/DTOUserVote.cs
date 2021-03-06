939401ab774fd2560a8ca2ccc5f294359c5d8939
Олег <oleg_vampiriys@mail.ru>
2021-03-06 19:22:48 +0300

    Создание ДТО, для безопасности данных

--- GameVote.Domain/DTO/DTOUserVote.cs
+++ GameVote.Domain/DTO/DTOUserVote.cs
@@ -1,24 +1,19 @@
-﻿using GameVote.Domain.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GameVote.Domain.DTO.IDTO;
+using GameVote.Domain.Entities;
 
-namespace GameVote.Domain.DTO.IDTO
+namespace GameVote.Domain.DTO
 {
-    public interface IDTOUserVote
+    class DTOUserVote : IDTOUserVote
     {
         public int Id { get; set; }
-
-        #region Game
         public Game Game { get; set; }
-
         public int GameId { get; set; }
-        #endregion
-
         public Store Store { get; set; }
         public DateTime date { get; set; }
         public decimal Price { get; set; }
-        
-        public int UserId { get; set; }     
+        public int UserId { get; set; }
     }
 }
---
+++ GameVote.Services/DTO/DTOGameForTitlePageServices.cs
@@ -0,0 +1,31 @@
+﻿using GameVote.Domain.DBServices.Interface;
+using GameVote.Domain.DTO;
+using System.Linq;
+
+namespace GameVote.Services.DTO
+{
+    public class DTOGameForTitlePageServices
+    {
+        private readonly IDBServices _iDBServices;
+        public DTOGameForTitlePageServices(IDBServices dBServices) => _iDBServices = dBServices;
+
+        public DTOGameForTitlePage Get(int id)
+        {
+
+            var games = _iDBServices.GetGamesForTitlePage();//.Where(i => i.Id == id);
+            var game = games.FindLast(i => i.Id == id);
+            //var userQuantityVote = _iDBServices.GetVoteForTitlePage();
+            return new DTOGameForTitlePage
+            {
+                Id = game.Id,
+                Name = game.Name,
+                UrlOfficialSaitGame = game.UrlOfficialSaitGame,
+                ImgGame = game.ImgGame,
+                Price = game.Price,
+                DiscountedPrice = game.discountedPrice,
+                UserVoteQuantity = games.Count(i => i.ModeGame != null),  // userQuantityVote.Count(i=>i.ModeGame!= null),
+                UserVoteFullPrice = games.Count(i => i.ModeGame != null)
+            };
+        }
+    }
+}
