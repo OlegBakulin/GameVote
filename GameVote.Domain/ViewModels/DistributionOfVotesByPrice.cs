f4ec1e6752b8070219ebc45c7ca3e497f519c48e
ilya <illf.yar@googlemail.com>
2021-03-07 16:57:03 +0300

    Доработал формирование модели списка игр для титульной страницы

---
+++ GameVote.Domain/ViewModels/DistributionOfVotesByPrice.cs
@@ -0,0 +1,15 @@
+﻿using GameVote.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameVote.Domain.ViewModels
+{
+    public class DistributionOfVotesByPrice
+    {
+        public int GameId { get; set; }
+        public int StoreId { get; set; }
+        public decimal Price { get; set; }
+        public int CountVotes { get; set; }
+    }
+}
