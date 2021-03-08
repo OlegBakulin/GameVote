e739092ac9adcc9ec2104450b6c48610aeac9693
Олег <oleg_vampiriys@mail.ru>
2021-03-08 14:52:50 +0300

    Полностью перенесён SQL

--- GameVote.Domain/DBServices/DBServices.cs
+++ GameVote.Domain/DBServices/DBServices.cs
@@ -6,7 +6,8 @@ using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using GameVote.Services.DBServices.Interface;
+
 
 namespace GameVote.Domain.DBServices
 {
